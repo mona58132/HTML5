e3083352b4f0109c368a4d16c300ce72a9c112d6
mona58132 <200258132@qu.edu.qa>
2015-10-25 16:09:48 +0300

    adding files

---
+++ js/DB.js
@@ -0,0 +1,26 @@
+﻿function OpenConn()
+{
+    var connection = new ActiveXObject("ADODB.Connection");
+
+    var connectionstring = "Data Source=MONA-PC\QSA;Initial Catalog=STARSDB;User ID=sa;Password=P@ssW0rd;Provider=SQLOLEDB";
+
+    connection.Open(connectionstring);
+    
+    //  return connection;
+    GetListofStudents(connection);
+}
+
+function GetListofStudents(connection) {
+    var rs = new ActiveXObject("ADODB.Recordset");
+    rs.Open("SELECT * FROM Students", connection);
+    rs.MoveFirst
+    while (!rs.eof) {
+        document.write(rs.fields(1));
+        rs.movenext;
+    }
+}
+function CloseConn(connection)
+{
+    rs.close;
+    connection.close;
+}
